4aad83da261e5790a07504c74cb033db88de4533
Kevin <Kevin@LAPTOP-79JRBJ58>
2018-02-12 15:53:27 -0600

    Item Class Updated and commited 2/12/18 3:53pm

--- Pathfinder Character Generator/project/pthfndr/src/main/Item.java
+++ Pathfinder Character Generator/project/pthfndr/src/main/Item.java
@@ -1,10 +1,17 @@
 package pthfndr.src.main;
+
+import java.util.ArrayList;
+
 public class Item implements Size {
-	public class Condition 
+	public interface Condition 
 	{
 		public final int PERFECT = 0, GOOD = 1, FAIR = 2, POOR = 3, BROKEN = 5, DESTROYED = 6;
 	}
-	public Item(String name, int Size, int Material, int cost, int weight, double thickness)
+	public Item()
+	{
+		
+	}
+	public Item(String name, int Size, int Material, double cost, double weight, double thickness)
 	{
 		this.setName(name);
 		this.setItemSize(Size);
@@ -29,99 +36,170 @@ public class Item implements Size {
 	private int age = 0; // how old the item is in years
 	private int dexMod = -5; // dex mod of an unattended item
 	private int accumulatedDamage; // holds accumulated damage for the aging fucntion
+	private boolean stackable;
+	private boolean massable;
 	
-	public void setName(String name)
-	{
+	public class Container extends Item {
+
+		public Container(String name, int Size, int Material, double cost, double weight, double thickness, double capacity) {
+			super(name, Size, Material, cost, weight, thickness);
+			this.setCapacity(capacity);
+		}
+		
+		private ArrayList<Item> contents = new ArrayList<>();
+		private double capacity;
+		
+		public double getCapacity() {
+			return this.capacity;
+		}
+		public void setCapacity(double capacity) {
+			this.capacity = capacity;
+		}
+		
+		public ArrayList<Item> getContents() {
+			return this.contents;
+		}
+		public void setContents(ArrayList<Item> contents) {
+			this.contents = contents;
+		}
+		public void storeItem(Item item) {
+			this.contents.add(item);
+		}
+		public void removeItem(Item item) {
+			for (int i = 0; i < this.contents.size(); i ++)
+				if (item.equals(this.contents.get(i)))
+				{
+					this.contents.remove(i);
+					return;
+				}
+		}
+		
+	}
+	
+	public class Stack extends Item {
+
+		public Stack(Item item, int count) {
+			super();
+			this.setType(item);
+			this.setCount(count);
+		}
+		
+		private Item type;
+		private int count;
+		
+		public void setType(Item type){
+			this.type = type;
+		}
+		public Item getType() {
+			return this.type;
+		}
+		
+		public void setCount(int count) {
+			this.count = count;
+		}
+		public int getCount() {
+			return this.count;
+		}
+		
+		//Adding and removing methods
+		public void addItem(Item item) {
+			if(item.equals(type)) {
+				this.count ++;
+			}
+		}
+		public void addItem(Stack stack) {
+			if(stack.equals(type)) {
+				this.count += stack.getCount();
+			}
+		}
+		public Item removeItem(Item item) {
+			if(item.equals(type)) {
+				this.count --;
+			}
+			return type;
+		}
+		public Stack splitStack(int count) {
+			Stack split = new Stack(this.type,count);
+			this.count -= count;
+			return split;
+		}
+		
+	}
+	
+	//name getters and setter
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
+	
+	//Armor Class getters and setters
 	public int getArmorClass() {
 		return armorClass;
 	}
-
-
 	public void setArmorClass() {
 		this.armorClass = 8 + Size.modifier(itemSize) - this.dexMod ;
 	}
-
-
+	
+	//Hit Point getters and setters
 	public int getCurrentHP() {
 		return currentHP;
 	}
-
-
 	public void damageItem(int damage) {
 		this.currentHP -= Math.max((damage - this.hardness),0);
 		if (this.currentHP < 0)
 			this.currentHP = 0;
 	}
-	public void repairItem(int repair){
+	public void repairItem(int repair) {
 		if (this.currentHP != this.maxHP)
 		this.currentHP += repair;
 		if (this.currentHP > this.maxHP)
 			this.currentHP = this.maxHP;
 	}
-
-
 	public int getMaxHP() {
 		return maxHP;
 	}
-
-
 	public void setMaxHP(int hp) {
 		this.maxHP = hp;
 	}
 	
-
+	//Hardness getters and setters
 	public int getHardness() {
 		return hardness;
 	}
-
-
 	public void setHardness(int hardness) {
 		this.hardness = hardness;
 	}
 
-
+	//Cost getters and setters
 	public double getCost() {
 		return cost;
 	}
-
-
 	public void setCost(double cost) {
 		this.cost = cost;
 	}
 
-
+	//Weight getters and setters
 	public double getWeight() {
 		return weight;
 	}
-
-
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
 
-
+	//Condition getters and setters
 	public int getCondition() {
 		return condition;
 	}
-
-
 	public void setCondition(int condition) {
 		this.condition = condition;
 	}
 
-
+	//Size getters and setters
 	public int getItemSize() {
 		return itemSize;
 	}
-
-
 	public void setItemSize(int itemSize) {
 		this.itemSize = itemSize;
 		if (this.itemSize < Size.FINE)
@@ -130,20 +208,36 @@ public class Item implements Size {
 		{this.itemSize = Size.COLOSSAL;}
 	}
 	
-	public int getMaterial()
-	{ return material; }
-	
-	public void setMaterial(int material)
-	{ 
+	//Material getters and setters
+	public int getMaterial() {
+		return material; 
+	}
+	public void setMaterial(int material) { 
 		this.material = material;
 		this.hardness = Material.hardness(material);
 		this.setMaxHP(Material.hitPoints(material, thickness));
 	}
+	
+	//Thickness getters and setters
 	public double getThickness()
 	{ return thickness;	}
 	
 	public void setThickness(double thickness)
 	{ this.thickness = thickness;}
+	
+	//boolean value getters and setters
+	public void setStackable(boolean value) {
+		this.stackable = value;
+	}
+	public boolean isStackable() {
+		return this.stackable;
+	}
+	public void setMassable(boolean value) {
+		this.massable = value;
+	}
+	public boolean isMassable() {
+		return massable;
+	}
 	
 	public void held(Creature creature)
 	{
